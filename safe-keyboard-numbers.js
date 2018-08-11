5e02043ad310215601458614ff4ce4ad730c4bec
Kejie Wen <kwen1000@users.noreply.github.com>
2018-08-10 23:41:45 -0700

    background highlight

--- safe-keyboard-numbers.js
+++ safe-keyboard-numbers.js
@@ -1,4 +1,8 @@
 /*
 Bookmarklet for safe input of digits into form fields. Useful for credit cards, etc.
+
+Hover over each number to input text.
+
+Does not work on mobile.
 */
-javascript:(function(){var div = document.createElement("div"); div.style.backgroundColor = "#EEEEEE"; div.style.zIndex = "999"; div.style.position = "fixed"; div.style.left = "16px"; div.style.top = "16px"; for (var i = 0; i < 10; i++){var a = document.createElement("a"); a.style.padding = "16px"; a.style.display = "inline-block"; a.innerText = ""+i; a.addEventListener("mouseover", function(event){document.activeElement.value += this.innerText}); div.appendChild(a)}; document.body.appendChild(div)}) ();
+javascript:(function(){var div = document.createElement("div"); div.style.backgroundColor = "#EEEEEE"; div.style.zIndex = "9999"; div.style.position = "fixed"; div.style.left = "16px"; div.style.top = "16px"; for (var i = 0; i < 10; i++){var a = document.createElement("a"); a.style.padding = "16px"; a.style.display = "inline-block"; a.innerText = ""+i; a.addEventListener("mouseover", function(event){document.activeElement.value += this.innerText; this.style.backgroundColor = "#BBBBBB"}); a.addEventListener("mouseover", function(event){this.style.backgroundColor = "#EEEEEE"}); div.appendChild(a)}; document.body.appendChild(div)}) ();
