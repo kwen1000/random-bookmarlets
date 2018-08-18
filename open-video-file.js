277d4dcc0499d5e8c5374fcda65a2430b9646632
Kejie Wen <kwen1000@users.noreply.github.com>
2018-08-17 21:04:29 -0700

    Update open-video-file.js

--- open-video-file.js
+++ open-video-file.js
@@ -1,1 +1,6 @@
+/*
+Opens main video file on new tab.
+Replace "video" with "img" to open main image file.
+*/
+
 javascript:window.open(document.getElementsByTagName("video")[0].src || "about:blank")
